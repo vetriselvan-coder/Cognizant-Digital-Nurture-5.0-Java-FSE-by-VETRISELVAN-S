29178f2cbdc53f7e3706266412267d057d755e53
vetriselvan-coder <vetriselvants@gmail.com>
2026-06-15 22:14:45 +0530

    Delete Digital_Nuture_5.0_FSE_Deepskilling/WEEK-1/Design patterns and principles directory

--- Digital_Nuture_5.0_FSE_Deepskilling/WEEK-1/Design patterns and principles/FactoryMethodPatternExample/ExcelDocumentFactory.java
+++
@@ -1,9 +0,0 @@
-package FactoryMethodPatternExample;
-
-public class ExcelDocumentFactory extends DocumentFactory {
-
-    @Override
-    public Document createDocument() {
-        return new ExcelDocument();
-    }
-}
--- Digital_Nuture_5.0_FSE_Deepskilling/WEEK-1/Design patterns and principles/FactoryMethodPatternExample/WordDocument.java
+++
@@ -1,8 +0,0 @@
-package FactoryMethodPatternExample;
-public class WordDocument implements Document {
-
-    @Override
-    public void open() {
-        System.out.println("Opening Word Document");
-    }
-}

b815938c06f85207b82ac9ce25e74c096cac9d50
vetriselvan-coder <vetriselvants@gmail.com>
2026-07-21 22:43:57 +0530

    Delete Digital_Nuture_5.0_FSE_Deepskilling/WEEK-1/WEEK 6/Backend/StudentCourse directory

--- Digital_Nuture_5.0_FSE_Deepskilling/WEEK-1/WEEK 6/Backend/StudentCourse/src/main/java/com/cognizant/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-
-}
